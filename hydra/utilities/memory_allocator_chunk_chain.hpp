4f4d8962a74ac9b42c2737f4ba58a45b6e1d2769
tim <timothee.feuillet@gmail.com>
2017-08-24 23:36:39 -0400

    [allocator-chunk-chain]: improvements / fixes
    - add checks for size=0 allocations
    - add checks for invalid allocations (bigger than a chunk size)
    - add a nice print_stats() function that print some stats

--- hydra/utilities/memory_allocator_chunk_chain.hpp
+++ hydra/utilities/memory_allocator_chunk_chain.hpp
@@ -43,7 +43,7 @@ namespace neam
       public:
         memory_allocator_chunk_chain() {}
         memory_allocator_chunk_chain(memory_allocator_chunk_chain &&o)
-          : memory_type_index(o.memory_type_index), allocator(o.allocator), 
+          : memory_type_index(o.memory_type_index), allocator(o.allocator),
             type(o.type), dev(o.dev),
             chunk_list(std::move(o.chunk_list)), dev_to_mac(std::move(o.dev_to_mac))
         {}
@@ -61,6 +61,9 @@ namespace neam
 
         memory_allocation allocate_memory(size_t size, size_t alignment)
         {
+          check::on_vulkan_error::n_assert(size < memory_allocator_chunk<>::chunk_allocation_size, "m_a_c_c::allocate(): cannot allocate more than the chunk size");
+          check::on_vulkan_error::n_assert(size != 0, "m_a_c_c::allocate(): cannot allocate nothing");
+
           for (auto &it : chunk_list)
           {
             if (it.first.can_allocate(size)) // fast heuristic to determinate if that chunk can allocate the requested memory
@@ -68,6 +71,7 @@ namespace neam
               int offset = it.first.allocate(size, alignment);
               if (offset == -1)
                 continue; // how unfortunate :/
+              ++allocation_count;
               return memory_allocation(memory_type_index, false, type, offset, size, &it.second, allocator);
             }
           }
@@ -77,6 +81,7 @@ namespace neam
           dev_to_mac.emplace(std::make_pair(&it.second, &it.first));
           int offset = it.first.allocate(size, alignment);
           check::on_vulkan_error::n_assert(offset != -1, "m_a_c_c::allocate(): allocation failed in an empty chunk. yay.");
+          ++allocation_count;
           return memory_allocation(memory_type_index, false, type, offset, size, &it.second, allocator);
         }
 
@@ -90,6 +95,8 @@ namespace neam
 
           mac->free(ma.offset(), ma.size());
 
+          --allocation_count;
+
           if (mac->is_empty()) // remove empty chunks
           {
             chunk_list.remove_if([&](const std::pair<memory_allocator_chunk<>, vk::device_memory> &_mac)
@@ -99,11 +106,36 @@ namespace neam
           }
         }
 
+        void print_stats(const std::string& name) const
+        {
+          size_t free_memory = 0;
+          size_t total_memory = 0;
+          size_t chunk_count = 0;
+          for (const auto& chunk : chunk_list)
+          {
+            free_memory += chunk.first.get_total_free_memory();
+            total_memory += chunk.second.get_size();
+            ++chunk_count;
+          }
+
+          if (chunk_count == 0)
+            return;
+
+          cr::out.log() << LOGGER_INFO_TPL("memory_allocator_chunk_chain", __LINE__) << "chain@" << this << "[" << name << "]:" << cr::newline
+                             << "  free memory:           " << (free_memory / (1024.f * 1024.f)) << " Mio" << cr::newline
+                             << "  allocated memory:      " << ((total_memory - free_memory) / (1024.f * 1024.f)) << " Mio" << cr::newline
+                             << "  total memory:          " << (total_memory / (1024.f * 1024.f)) << " Mio" << cr::newline
+                             << "  number of allocations: " << allocation_count << cr::newline
+                             << "  number of chunk:       " << (chunk_count) << std::endl;
+        }
+
       private:
         uint32_t memory_type_index = 0;
         memory_allocator *allocator = nullptr;
         allocation_type type;
         vk::device *dev;
+
+        size_t allocation_count = 0;
 
         std::list<std::pair<memory_allocator_chunk<>, vk::device_memory>> chunk_list; // normal chunk list
         std::unordered_map<const vk::device_memory *, memory_allocator_chunk<> *> dev_to_mac;
