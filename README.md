bec6daa4b101846078f2e31c0c21f5646d43d351
tim42 <tim42@users.noreply.github.com>
2024-11-28 22:44:12 -0500

    Update readme

--- README.md
+++ README.md
@@ -12,90 +12,30 @@ hydra: C++23ish vulkan renderer(ish).
 ---
 
 **Disclaimer**: the only supported platform is linux (not unix, linux), eventhough other unix platforms
-can be accomodated for at a loss of feature.
+can be accomodated for at a loss of feature. (And windows can be supported when the liburing equivalent is available)
 
  - Hydra uses lib_uring for asynchronous IO. Other POSIX platforms can fallback to a fully synchronous IO
    (they should support scatter/gatherIO, otherwise it's going to be synchronous AND slower than necessary).
    (synchronous fallback is not done as of yet).
- - Hydra uses some linux-ism for loading modules from packed files.
-   There's a least a BSD fallback that I know of.
- - Hydra uses makefiles and bash scripts for the build system. Platforms with poor support for those will have
-   poor support for hydra.
 
 I do not own a window/bsd/\*nix machine with a window/bsd/\*nix dev env, and I don't plan to.
-As such I am unable to add windows/bsd/\*nix support.
+As such I am unable to add proper windows/bsd/\*nix support.
 
 ## Engine Features
 
- - Fully asynchronous and easy to use IO. (and _only_ asynchronous IO).
- - A modern and flexible (and fully asynchronous) resource system.
- - A fully reloadable / dynamic code and data handling (code is a resource).
- - 
-
-
+ - Fully asynchronous and easy to use IO. (and _only_ asynchronous IO)
+ - A modern, flexible (and fully asynchronous) resource system
+ - Fully task based. Most things will run in parallel.
+ - A very nice and modern renderer
+ - A nice way to handle configuration (replaces console variables & conf in most engines), albeit with a binary format.
+   (It uses RLE + metadata. But the format is self descripting and generates nice UI / can be logged / ...)
+ - Automatic UI generation from C++ types. (Again, it uses RLE facilities for that).
 
 This project is under development, and can't be used in any kind of project.
 Yet.
 
 Also keep in mind that this project is mainly for me a way to learn vulkan, so
 the early code may be... somewhat strange and quite odd.
-
-## Features
-
-- ultra-verbose / comprehensive error reporting / debug logging
-  (can log each call to vulkan that return a value)
-- some kind of a vulkan wrapper, with some missing features and bad
-  implementation at some places. (wait, is _that_ a feature ?).
-- A super nice vulkan instance and device creator that is way easier to use
-  than the ultra-verbose vulkan way
-- a nice (yet untested) scheduler designed to be extremely adaptable to most
-  usages (with a thread pool ? ok. Oh and I have a thread with 5ms of free time ?
-  Ok. That task should only run on those two thread, depending on whose has less
-  work, and with a delay of 12ms. Ok. Oh and If a thread have some free time, I
-  would like to perform some background task without blocking or slowing down
-  anything, just for using a bit more the CPU when there's nothing else. Ok.
-  Is that also possible to tell me if I should add another thread ? Ok). And
-  much more.
-- An utility to batch transfers (currently, it only handle buffers, but may
-  soon handle images
-- Header only. (ok, that may be a bad idea and make crash my IDE).
-
-Hydra isn't tied to a window system, and is designed to have almost no
-dependencies (excepted a C++14 compiler, vulkan and glm), and thus can be used
-as a offline renderer. It provides nevertheless a GLFW extension that allows
-and easy window creation and management.
-
-And more importantly, it allow you to perform most vulkan tutorials in less than
-~150 lines while providing the same functionality.
-
-## Future
-
-Broken code will hopefully be fixed ; a higher level "layer" will be present ;
-PBR / PPR extensions ; texture and model loader extensions ;
-YägGLer application-like concept ; and much more.
-
-## Non-Future
-
-_What I don't want in hydra_
-
-
-Copy. For most objects, copy (affectation or copy construction) is disabled.
-The only thing you can do use the move sematic on objects (mostly via
-constructors, sometimes via affectation).<br/>
-(copy can be a heavy or meaningless operation when done with the = operator,
-as, with vulkan, most of the time you require a queue, a command buffer,
-sometime synchronisation and memory barriers for transitions. To keep things
-simples, you can't copy except by explicitly calling a method for that).
-
-
-Having a non-extensible / non-easily modifiable API: you want to do
-something directly with vulkan ? ok. You then want to use the result of that
-operation with hydra ? ok. You better know how to use vulkan, but that will
-work.
-
-This also means that hydra can't be a heavy interface-based API and will expose
-/ be shaped around some vulkan particularities.
-
 
 ---
 
--- samples/basic/fs_quad_pass.hpp
+++ samples/basic/fs_quad_pass.hpp
@@ -28,6 +28,7 @@
 
 #include <ntools/container_utils.hpp>
 #include <hydra/hydra.hpp>          // the main header of hydra
+#include "shader_struct.hpp"
 // #include "app.hpp"
 
 /// \brief A dummy vertex, just for fun
@@ -63,37 +64,10 @@ namespace neam
 {
   inline void make_fs_quad_pipeline(hydra::hydra_context& context, hydra::pipeline_render_state& prs)
   {
-    hydra::vk::descriptor_set_layout ds_layout =
-    {
-      context.device,
-      {
-        neam::hydra::vk::descriptor_set_layout_binding(0, VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER, 1, VK_SHADER_STAGE_FRAGMENT_BIT),
-        neam::hydra::vk::descriptor_set_layout_binding(1, VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER, 1, VK_SHADER_STAGE_FRAGMENT_BIT),
-      }
-    };
-    hydra::vk::pipeline_layout p_layout =
-    {
-      context.device, { &ds_layout }
-    };
-
-    prs.create_pipeline_info
-    (
-      std::move(ds_layout),
-      hydra::vk::descriptor_pool
-      {
-        context.device, 100,
-        {
-          {VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER, 1},
-          {VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER, 1},
-        }
-      },
-      std::move(p_layout)
-    );
-
-    hydra::vk::pipeline_creator& pcr = prs.get_pipeline_creator();
+    hydra::vk::graphics_pipeline_creator& pcr = prs.get_graphics_pipeline_creator();
     pcr.get_pipeline_shader_stage()
-      .add_shader(context.shmgr.load_shader("shaders/2d_plane.hsf:spirv(main_vs)"_rid), VK_SHADER_STAGE_VERTEX_BIT)
-      .add_shader(context.shmgr.load_shader("shaders/2d_plane.hsf:spirv(main_fs)"_rid), VK_SHADER_STAGE_FRAGMENT_BIT);
+      .add_shader(context.shmgr.load_shader("shaders/2d_plane.hsf:spirv(main_vs)"_rid))
+      .add_shader(context.shmgr.load_shader("shaders/2d_plane.hsf:spirv(main_fs)"_rid));
 
     pcr.get_viewport_state()
       .set_dynamic_viewports_count(1)
@@ -108,7 +82,6 @@ namespace neam
       static constexpr unsigned int logo_size = 1024;
       fs_quad_pass(hydra::hydra_context& _context)
         : hydra::render_pass(_context),
-          vk_render_pass(context.device),
           mesh(context.device),
 
           hydra_logo_img
@@ -123,28 +96,13 @@ namespace neam
               )
             )
           ),
-           sampler(context.device, VK_FILTER_NEAREST, VK_FILTER_LINEAR, VK_SAMPLER_MIPMAP_MODE_LINEAR, 0.f, 0.f, 0.f),
-           uniform_buffer(context.device, neam::hydra::vk::buffer(context.device, 100, VK_BUFFER_USAGE_TRANSFER_DST_BIT | VK_BUFFER_USAGE_UNIFORM_BUFFER_BIT))
+          sampler(context.device, VK_FILTER_NEAREST, VK_FILTER_LINEAR, VK_SAMPLER_MIPMAP_MODE_LINEAR, 0.f, 0.f, 0.f),
+          uniform_buffer(context.device, sizeof(fs_quad_ubo), VK_BUFFER_USAGE_TRANSFER_DST_BIT | VK_BUFFER_USAGE_UNIFORM_BUFFER_BIT)
       {
       }
 
       void setup(hydra::render_pass_context& rpctx) override
       {
-        screen_resolution = (glm::vec2)rpctx.output_size;
-
-        //////////////////////////////////////////////////////////////////////////////
-        // create the render pass
-        vk_render_pass.create_subpass().add_attachment(neam::hydra::vk::subpass::attachment_type::color, VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL, 0);
-        vk_render_pass.create_subpass_dependency(VK_SUBPASS_EXTERNAL, 0)
-          .dest_subpass_masks(VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT, VK_ACCESS_COLOR_ATTACHMENT_WRITE_BIT)
-          .source_subpass_masks(VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT, 0);
-
-        vk_render_pass.create_attachment().set_format(rpctx.output_format).set_samples(VK_SAMPLE_COUNT_1_BIT)
-          .set_load_op(VK_ATTACHMENT_LOAD_OP_LOAD, VK_ATTACHMENT_LOAD_OP_DONT_CARE)
-          .set_store_op(VK_ATTACHMENT_STORE_OP_STORE, VK_ATTACHMENT_STORE_OP_DONT_CARE)
-          .set_layouts(VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL, VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL);
-        vk_render_pass.refresh();
-
         //////////////////////////////////////////////////////////////////////////////
         // setup the mesh
         mesh.add_buffer(sizeof(indices[0]) * indices.size(), VK_BUFFER_USAGE_TRANSFER_DST_BIT | VK_BUFFER_USAGE_INDEX_BUFFER_BIT);
@@ -152,31 +110,29 @@ namespace neam
         mesh.vertex_input_state() = dummy_vertex::get_vertex_input_state();
         mesh.allocate_memory(context.allocator);
 
-        mesh.transfer_data(context.transfers, 0, sizeof(indices[0]) * indices.size(), indices.data(), context.gqueue.get_queue_familly_index());
-        mesh.transfer_data(context.transfers, 1, sizeof(vertices[0]) * vertices.size(), vertices.data(), context.gqueue.get_queue_familly_index());
+        mesh.transfer_data(rpctx.transfers, 0, raw_data::allocate_from(indices), context.gqueue);
+        mesh.transfer_data(rpctx.transfers, 1, raw_data::allocate_from(vertices), context.gqueue);
 
         //////////////////////////////////////////////////////////////////////////////
         // allocate memory for the image (+ transfer data to it)
         {
           hydra_logo_img_allocation = context.allocator.allocate_memory(hydra_logo_img.get_memory_requirements(), VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT, neam::hydra::allocation_type::persistent_optimal_image);
           hydra_logo_img.bind_memory(*hydra_logo_img_allocation.mem(), hydra_logo_img_allocation.offset());
+          hydra_logo_img._set_debug_name("fs-quad/hydra_logo_img");
 
-          uint8_t *pixels = new uint8_t[logo_size * logo_size * 4];
-          neam::hydra::generate_rgba_logo(pixels, logo_size, 5, 0xFFFFFF);
-          context.transfers.add_transfer(hydra_logo_img, VK_IMAGE_LAYOUT_UNDEFINED, VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL, logo_size * logo_size * 4, pixels);
-          delete[] pixels;
+          raw_data pixels = raw_data::allocate(logo_size * logo_size * 4);
+          neam::hydra::generate_rgba_logo((uint8_t*)pixels.get(), logo_size, 5, 0xFFFFFF);
+          rpctx.transfers.acquire(hydra_logo_img);
+          rpctx.transfers.transfer(hydra_logo_img, std::move(pixels));
+          rpctx.transfers.release(hydra_logo_img, context.gqueue, VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL);
         }
         //////////////////////////////////////////////////////////////////////////////
         // allocate memory for the uniform buffer (+ transfer data to it)
         {
-          uniform_buffer.set_transfer_info(context.transfers, context.tqueue, context.vrd);
-          uniform_buffer_allocation = context.allocator.allocate_memory(uniform_buffer.get_buffer().get_memory_requirements(), VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT, neam::hydra::allocation_type::persistent);
+          uniform_buffer_allocation = context.allocator.allocate_memory(uniform_buffer.get_memory_requirements(), VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT, neam::hydra::allocation_type::persistent);
 
-          uniform_buffer.get_buffer().bind_memory(*uniform_buffer_allocation.mem(), uniform_buffer_allocation.offset());
-
-          size_t offset = 0;
-          offset = uniform_buffer.watch(time, offset, neam::hydra::buffer_layout::std140);
-          offset = uniform_buffer.watch(screen_resolution, offset, neam::hydra::buffer_layout::std140);
+          uniform_buffer.bind_memory(*uniform_buffer_allocation.mem(), uniform_buffer_allocation.offset());
+          uniform_buffer._set_debug_name("fs-quad/uniform_buffer");
         }
 
         hydra_logo_img_view = decltype(hydra_logo_img_view)(new neam::hydra::vk::image_view(context.device, hydra_logo_img, VK_IMAGE_VIEW_TYPE_2D));
@@ -184,54 +140,82 @@ namespace neam
 
         // create the pipeline
         context.ppmgr.add_pipeline("hydra-logo"_rid, [this](auto& p) { make_fs_quad_pipeline(context, p); });
-        descriptor_set = context.ppmgr.allocate_descriptor_set("hydra-logo"_rid);
 
-        // update ds
-        descriptor_set.write_descriptor_set(0, 0, VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER, {{ sampler, *hydra_logo_img_view, VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL }});
-        descriptor_set.write_descriptor_set(1, 0, VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER, {{ uniform_buffer, uniform_buffer.get_buffer_offset(), uniform_buffer.get_area_size() }});
+        // logo_index = context.textures.request_texture_index("images/hydra-logo.png.xz/hydra-logo.png:image"_rid);
+        logo_index = context.textures.request_texture_index("images/hydra-logo-square.png:image"_rid);
       }
 
       void prepare(hydra::render_pass_context& rpctx) override
       {
-        time = cr::chrono::now_relative();
-        screen_resolution = (glm::vec2)rpctx.output_size;
+        // prevent the logo from being streamed-out
+        context.textures.indicate_texture_usage(logo_index, 0);
 
-        uniform_buffer.sync();
+        rpctx.transfers.acquire(uniform_buffer, context.gqueue);
+        rpctx.transfers.transfer(uniform_buffer, raw_data::duplicate(
+        fs_quad_ubo
+        {
+          .time = (float)cr::chrono::now_relative(),
+          .screen_resolution = (glm::vec2)rpctx.output_size,
+          .logo_index = context.textures.texture_index_to_gpu_index(logo_index),
+        }));
+        rpctx.transfers.release(uniform_buffer, context.gqueue);
+
+        {
+          descriptor_set.tex_sampler = {*hydra_logo_img_view, sampler};
+          descriptor_set.ubo = uniform_buffer;
+          descriptor_set.update_descriptor_set(context);
+        }
       }
 
       hydra::render_pass_output submit(hydra::render_pass_context& rpctx) override
       {
-        hydra::vk::command_buffer cmd_buf = graphic_transient_cmd_pool.create_command_buffer();
+        hydra::vk::command_buffer cmd_buf = context.gcpm.get_pool().create_command_buffer();
+        cmd_buf._set_debug_name("fs-quad::command_buffer");
         hydra::vk::command_buffer_recorder cbr = cmd_buf.begin_recording(VK_COMMAND_BUFFER_USAGE_ONE_TIME_SUBMIT_BIT);
-
-        cbr.pipeline_barrier(VK_PIPELINE_STAGE_TRANSFER_BIT, VK_PIPELINE_STAGE_VERTEX_INPUT_BIT, 0,
+        // if (0)
         {
-          neam::hydra::vk::buffer_memory_barrier::queue_transfer(uniform_buffer.get_buffer(), context.transfers.get_queue_familly_index(), context.gqueue.get_queue_familly_index()),
-        });
+          neam::hydra::vk::cbr_debug_marker _dm(cbr, "fsquad");
 
-        cbr.begin_render_pass(vk_render_pass, rpctx.final_fb, hydra::vk::rect2D(glm::ivec2(0, 0), (glm::ivec2)screen_resolution), VK_SUBPASS_CONTENTS_INLINE, { glm::vec4(0.0f, 0x89 / 255.f, 1.0f, 0.f) });
-        cbr.bind_pipeline(context.ppmgr.get_pipeline("hydra-logo"_rid, vk_render_pass, 0, mesh));
-        cbr.set_viewport({rpctx.viewport}, 0, 1);
-        cbr.set_scissor(rpctx.viewport_rect);
+  //         cbr.pipeline_barrier(VK_PIPELINE_STAGE_TRANSFER_BIT, VK_PIPELINE_STAGE_VERTEX_INPUT_BIT, 0,
+  //         {
+  //           neam::hydra::vk::buffer_memory_barrier::queue_transfer(uniform_buffer.get_buffer(), rpctx.transfers.get_queue_familly_index(), context.gqueue.get_queue_familly_index()),
+  //         });
 
-        cbr.bind_descriptor_set(VK_PIPELINE_BIND_POINT_GRAPHICS, context.ppmgr.get_pipeline_layout("hydra-logo"_rid), 0, { &descriptor_set });
-        mesh.bind(cbr);
-        // TODO: a draw state, like in YägGLer
-        cbr.draw_indexed(indices.size(), 1, 0, 0, 0);
-        cbr.end_render_pass();
+          rpctx.begin_rendering(cbr, VK_ATTACHMENT_LOAD_OP_CLEAR, VK_ATTACHMENT_STORE_OP_STORE);
+
+          cbr.bind_graphics_pipeline(context.ppmgr, "hydra-logo"_rid, mesh,
+                                                      hydra::vk::specialization
+                                                      {{
+                                                        { "loop_count_factor"_rid, float(2) },
+                                                      }});
+          cbr.set_viewport({rpctx.viewport}, 0, 1);
+          cbr.set_scissor(rpctx.viewport_rect);
+
+          // cbr.bind_descriptor_set(VK_PIPELINE_BIND_POINT_GRAPHICS, context.ppmgr.get_pipeline_layout("hydra-logo"_rid), 0, { &descriptor_set });
+          cbr.bind_descriptor_set(context, descriptor_set);
+          cbr.bind_descriptor_set(context, context.textures.get_descriptor_set());
+          mesh.bind(cbr);
+          cbr.draw_indexed(indices.size(), 1, 0, 0, 0);
+          cbr.end_rendering();
+        }
         cmd_buf.end_recording();
 
         return { .graphic = cr::construct<std::vector>(std::move(cmd_buf)) };
       }
 
+      void cleanup(hydra::render_pass_context& rpctx) override
+      {
+        TRACY_SCOPED_ZONE;
+
+        context.dfe.defer_destruction(context.dfe.queue_mask(context.gqueue), descriptor_set.reset());
+      }
+
       neam::hydra::vk::image_view* get_hydra_logo_img_view() { return hydra_logo_img_view.get(); }
-
     private:
-      neam::hydra::vk::render_pass vk_render_pass;
-
       neam::hydra::mesh mesh;
 
-      neam::hydra::vk::descriptor_set descriptor_set { context.device, VkDescriptorSet(nullptr) };
+      fs_quad_shader_params descriptor_set;
+      // neam::hydra::vk::descriptor_set descriptor_set { context.device, VkDescriptorSet(nullptr) };
 
       neam::hydra::vk::image hydra_logo_img;
       neam::hydra::memory_allocation hydra_logo_img_allocation;
@@ -239,12 +223,8 @@ namespace neam
 
       std::unique_ptr<neam::hydra::vk::image_view> hydra_logo_img_view;
 
-      neam::hydra::auto_buffer uniform_buffer;
+      neam::hydra::vk::buffer uniform_buffer;
       neam::hydra::memory_allocation uniform_buffer_allocation;
-
-      // uniforms
-      float time = 0.5f;
-      glm::vec2 screen_resolution = 900_vec2_xy;
-
+      neam::hydra::texture_index_t logo_index;
   };
 }
